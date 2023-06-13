6bda8a28f80ad17e630501faa1f032a624167b2a
Victor Michael <victormikel98@gmail.com>
2023-06-13 16:25:11 +0100

    All the lines now start with the first verse

--- Clean_up1.py
+++ Clean_up1.py
@@ -1,15 +1,14 @@
 with open('out_new.txt', 'r', errors='ignore') as file:
     lines = file.readlines()
-keyword = 'n1'
-select_word = 'n1'
+keyword = '1.'
+select_word = '1.'
 modified_lines = []
 for line in lines:
     if keyword in line:
         index = line.find(select_word)
-        new_line = line[index+len(select_word):]
-        modified_lines.append(new_line)
+        modified_lines.append(line[index:])
     else:
         modified_lines.append(line)
-with open('out_new_new.txt', 'w') as file:
+with open('out_new_new1.txt', 'w') as file:
     file.writelines(modified_lines)
 print('Output file created successfully!')
